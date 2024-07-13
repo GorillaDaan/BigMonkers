b363963ffa92d91a02f7995aa4d37f4177572ce2
LiquidScammer <liquidscamer@gmail.com>
2024-07-13 15:59:48 -0700

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.LiquidScammer.gorillatag.BigMonkers";
+		public const string Name = "BigMonkers";
+		public const string Version = "1.0.0";
+	}
+}
